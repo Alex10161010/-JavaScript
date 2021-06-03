21be005aacc299ae37f1dec6f9a7da6efc9c4701
Alex10161010 <eliasalejandromome@gmail.com>
2021-06-03 10:14:07 -0500

    verificar si exite elemento dentro de un arreglo

---
+++ arreglos/validarExiste.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let arrayDatos = [120, 130, 250, 560, 145];
+	const exite = validaExiteOtrosZ(arrayDatos);
+	console.log(exite);
+});
+
+/* No si cumple la condicion sale del ciclo*/
+const validaExiteOtrosX = (arraySelect) => {
+	const arrayIdOtros = [128, 132, 136, 150, 158, 141, 147];
+	let existe = false;
+	for (let i = 0; i < arraySelect.length; i++) {
+		if (arrayIdOtros.some((elem) => elem === arraySelect[i])) {
+			existe = true;
+			break;
+		}
+	}
+	return existe;
+};
+
+/* No se puede ocupar break, realiza todo el recorrido*/
+const validaExiteOtrosY = (arraySelect) => {
+	const arrayIdOtros = [128, 132, 136, 150, 158, 141, 147];
+	let existe = false;
+	arraySelect.forEach((select) => {
+		if (arrayIdOtros.some((elem) => elem === select)) {
+			existe = true;
+		}
+	});
+	return existe;
+};
+
+const validaExiteOtrosZ = (arraySelect) => {
+	const arrayIdOtros = [128, 132, 136, 150, 158, 141, 147];
+	let existe = false;
+	arraySelect.forEach((elem) => {
+		if (arrayIdOtros.includes(elem)) {
+			existe = true;
+		}
+	});
+	return existe;
+};
