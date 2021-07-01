80b11051924169dd38f46c098d1727cff5e614ac
Alex10161010 <eliasalejandromome@gmail.com>
2021-06-30 23:28:00 -0500

    como realizar una desestructuracion

--- desestructuracion/app.js
+++ desestructuracion/app.js
@@ -14,7 +14,39 @@ const data = {
 	],
 };
 
-let platicas = data.platicas;
-for (let i = 0; i < platicas.length; i++) {
-	console.log(platicas[i].salon);
+const deadpool = {
+	nombre: 'Wade',
+	apellido: 'Winston',
+	poder: 'Regeneracion',
+	getNombre() {
+		return `${this.nombre} ${this.apellido} ${this.poder}`;
+	},
+};
+
+/*
+desestructuración del objecto
+const { nombre, apellido, poder, edad = 0 } = deadpool;
+	console.log(nombre, apellido, poder, edad);
+*/
+
+/* Desestructuración en los argumentos */
+function imprimeHeroe({ nombre, apellido, poder, edad = 50 }) {
+	console.log(nombre, apellido, poder, edad);
 }
+
+/*  Desestructuración de arreglos*/
+const heroes = [
+	'Chica Maravilla',
+	'Deadpool',
+	'Supermen',
+	'Batmant',
+	'Spiderman',
+	'Ironman',
+	'Doctor Strange',
+];
+
+const [, , , batmant] = heroes;
+console.log(batmant);
+
+const [h1, h2, ...restoheros] = heroes;
+console.log(restoheros);
