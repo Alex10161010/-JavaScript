8cd368530123268fb1d3dc2a2daee28ad90cf292
Alex10161010 <eliasalejandromome@gmail.com>
2021-10-20 18:08:22 -0500

    agregando modulos

---
+++ modulo/modulo_base.js
@@ -0,0 +1,3 @@
+// import { nombre } from './modulo_dos.mjs';
+import defau from './modulo_dos.mjs';
+console.log(defau);
---
+++ modulo/modulo_dos.mjs
@@ -0,0 +1,4 @@
+export const nombre = 'Elias';
+let valorDefecto = 'Alejandro Morales';
+
+export default valorDefecto;
